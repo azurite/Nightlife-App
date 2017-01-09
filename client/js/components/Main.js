5c2f2e8e4ae5d8f0304e3987f3dff406407b570f
Marko Nikic <darkblue.azurite@gmail.com>
2017-01-09 22:54:18 +0100

    start to implement search bar

--- client/js/components/Main.js
+++ client/js/components/Main.js
@@ -1,11 +1,26 @@
 const React = require("react");
-const { Grid } = require("react-bootstrap");
+const { Grid, Row, Col, Button, Form, FormGroup, InputGroup, FormControl } = require("react-bootstrap");
 
 const Main = React.createClass({
   render: function() {
     return (
       <Grid fluid>
-        <h1>Test</h1>
+        <Row>
+          <Col sm={8} xs={10} smOffset={2} xsOffset={1}>
+            <h1 className="title-main">Where are you going tonight?</h1>
+            <Form>
+              <FormGroup>
+                <InputGroup>
+                  <InputGroup.Addon><i className="fa fa-search"></i></InputGroup.Addon>
+                  <FormControl type="text" name="query"/>
+                  <InputGroup.Button>
+                    <Button>Go!</Button>
+                  </InputGroup.Button>
+                </InputGroup>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
       </Grid>
     );
   }
