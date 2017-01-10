af8e5b9d973ef0ead4c6a352179f11a0e7ef53bb
Marko Nikic <darkblue.azurite@gmail.com>
2017-01-10 22:42:32 +0100

    complete venue cards

--- client/js/components/VenueCard.js
+++ client/js/components/VenueCard.js
@@ -1,24 +1,30 @@
 const React = require("react");
-const { Img, Col } = require("react-bootstrap");
+const { Link } = require("react-router");
+const { Image, Row, Col } = require("react-bootstrap");
 
 const VenueCard = function(props) {
   return (
-    <div className="venue-card">
-      <Col xs={3}>
-        <Img src={props.image_url} responsive circle className="venue-card vc-img"/>
-      </Col>
-      <Col xs={9}>
-        <h3 className="venue-card vc-name">{props.name}</h3>
-        <p className="venue-card vc-snippet">{props.snippet_text}</p>
-      </Col>
-    </div>
+    <Link to={"/venue/" + props.id}>
+      <div className="venue-card">
+        <Row>
+          <Col xs={3}>
+            <Image src={props.image_url} responsive circle className="venue-card vc-img"/>
+          </Col>
+          <Col xs={9}>
+            <h3 className="venue-card vc-name">{props.name}</h3>
+            <p className="venue-card vc-snippet">{props.snippet_text}</p>
+          </Col>
+        </Row>
+      </div>
+    </Link>
   );
 };
 
 VenueCard.propTypes = {
   image_url: React.PropTypes.string,
   name: React.PropTypes.string,
-  snippet_text: React.PropTypes.string
+  snippet_text: React.PropTypes.string,
+  id: React.PropTypes.string.isRequired
 };
 
 module.exports = VenueCard;
