77827fa0aac6a54037b28b653866809c45d7a269
Marko Nikic <darkblue.azurite@gmail.com>
2017-01-11 23:34:34 +0100

    add venue detail view

--- client/js/components/VenueDetail.js
+++ client/js/components/VenueDetail.js
@@ -1,12 +1,68 @@
 const React = require("react");
+const { Grid, Row, Col, Image, Button } = require("react-bootstrap");
+
+const User = function(props) {
+  return (
+    <div className="is-going-user">
+      <Image src={props.url} circle responsive/>
+      <span>{props.name}</span>
+    </div>
+  );
+};
+
+User.propTypes = {
+  url: React.PropTypes.string,
+  name: React.PropTypes.string
+};
 
 const VenueDetail = React.createClass({
   propTypes: {
-    params: React.PropTypes.object
+    params: React.PropTypes.object,
+    route: React.PropTypes.object
+  },
+  googleMaps: function() {
+    var baseUrl = "https://maps.google.com/?q=";
+    return baseUrl + this.props.route.sample.display_address.join();
+  },
+  phone: function() {
+    return "tel:" + this.props.route.sample.phone;
   },
   render: function() {
+    var venue = this.props.route.sample;
     return (
-      <h1>Venue Detail View of: {this.props.params.id}</h1>
+      <Grid fluid>
+          <Row>
+            <Col sm={8} xs={10} smOffset={2} xsOffset={1} className="venue-detail">
+              <Row>
+                <Col sm={4} xs={12} className="venue-info">
+                  <Image src={venue.image_url} circle responsive/>
+                  <a href={this.phone()}>
+                    <Button className="btn-red btn-block btn-edge">
+                      <i className="fa fa-phone"></i> {venue.display_phone}
+                    </Button>
+                  </a>
+                  <a href={this.googleMaps()} target="_blank">
+                    <Button className="btn-red btn-block btn-edge">
+                      Show on Map <i className="fa fa-map-marker"></i>
+                    </Button>
+                  </a>
+                </Col>
+                <Col sm={8} xs={12} className="v-center-title">
+                  <h1 className="title-main">{venue.name}</h1>
+                  <Button className="btn-red btn-edge border-white align-right">Go there tonight</Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="last-row">
+            <Col sm={8} xs={10} smOffset={2} xsOffset={1} className="is-going-list">
+              <h3 className="text-center">{venue.is_also_going.length} people are going there tonight</h3>
+              {venue.is_also_going.map((u, i) => {
+                return <User key={i} url={u.image_url} name={u.username}/>;
+              })}
+            </Col>
+          </Row>
+      </Grid>
     );
   }
 });
--- client/js/routes.js
+++ client/js/routes.js
@@ -1,6 +1,8 @@
 const React = require("react");
 const { Route, IndexRoute } = require("react-router");
 
+const sample = require("./dev/sample_venue");
+
 const App = require("./components/App");
 const Main = require("./components/Main");
 const Login = require("./components/Login");
@@ -11,7 +13,7 @@ const routes = function(store) {
     <Route path="/" component={App}>
       <IndexRoute component={Main}/>
       <Route path="/login" component={Login}/>
-      <Route path="/venue/:id" component={VenueDetail}/>
+      <Route path="/venue/:id" component={VenueDetail} sample={sample}/>
     </Route>
   );
 };
