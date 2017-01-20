23fd01c6d137227f70c0e9a5b7261850d2de4a45
Marko Nikic <darkblue.azurite@gmail.com>
2017-01-20 22:09:51 +0100

    add register post route

--- app/api/user.js
+++ app/api/user.js
@@ -1,8 +1,28 @@
+const Account = require("../../models/user");
 const express = require("express");
 const router = express.Router();
 
 //Todo:
-//login, logout, register, go to venue, remove from venue
+//login
+//logout
+//register (done)
+//go to venue
+//remove from venue
+
+router.post("/api/user/register", (req, res) => {
+  var local = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  Account.register({ local: local }, req.body.password, (err, user) => {
+    if(err) {
+      res.json({ statusCode: 500, error: { message: err.message } });
+      return;
+    }
+    res.json(user.normalize());
+  });
+});
 
 module.exports = function() {
   return router;
